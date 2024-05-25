00271845561b25cff7b25f410cd6c7864fc180e3
Samir Pandey <80725629+venawm@users.noreply.github.com>
2024-05-25 19:38:12 +0545

    Add decodeURI to process the UTF-8 character representation (#2)

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 // Import required modules
-const { name } = require('ejs');
-const express = require('express');
-const path = require('path');
+const { name } = require("ejs");
+const express = require("express");
+const path = require("path");
 
 // Create an Express application
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Define a route to serve your homepage
-app.get('/', (req, res) => {
-    // Send the HTML file as the response
-    res.render("home")
+app.get("/", (req, res) => {
+  // Send the HTML file as the response
+  res.render("home");
 });
-app.get('/:name', (req, res) => {
-    let name = req.params.name
-    name = name.charAt(0).toUpperCase() + name.slice(1)
-    res.render("index", {name})
+app.get("/:name", (req, res) => {
+  let name = req.params.name;
+  name = name.charAt(0).toUpperCase() + name.slice(1);
+  res.render("index", { name });
 });
 
 // Start the server
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
